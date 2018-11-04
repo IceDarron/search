402b0cbfdd009d95537d51871649c15da2d653fd
IceDarron <darron_r@163.com>
2018-11-05 00:38:53 +0800

    当前版本:5a903fe16e05df66ed5c4da139de0009893fec31 回退版本:215bad74f53adb4851b69034e5f3b5e555c6bc28
    回退原因:由于整体结构使用springboot2.0，因此应以spring-boot-starter-data系列的依赖为主，学习springboot下的快捷开发。
    因此将一开始的demo代码重新拷贝到当前版本下。目前两者代码均存在，尚未调试。此后以springboot模式开发，将一些elasticsearch相关的较为复杂的查询尝试自己封装成工具类。

---
+++ src/main/java/com/icedarron/search/connection/ElasticsearchConfiguration.java
@@ -0,0 +1,65 @@
+package com.icedarron.search.connection;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import java.net.InetAddress;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "com.icedarron.search.repository")
+public class ElasticsearchConfiguration {
+
+
+    @Value("${elasticsearch.host}")
+    private String EsHost;
+
+    @Value("${elasticsearch.port}")
+    private int EsPort;
+
+    @Value("${elasticsearch.clustername}")
+    private String EsClusterName;
+
+    @Bean
+    public Client client() throws Exception {
+
+        Settings esSettings = Settings.builder()
+                .put("cluster.name", EsClusterName)
+                .put("thread_pool.search.size", 20)// 增加线程池个数，暂时设为5
+                .build();
+
+
+//        //https://www.elastic.co/guide/en/elasticsearch/guide/current/_transport_client_versus_node_client.html
+          TransportClient preBuiltTransportClient = new PreBuiltTransportClient(esSettings);
+//        PreBuiltTransportClient preBuiltTransportClient = new PreBuiltTransportClient(esSettings);
+        TransportAddress inetSocketTransportAddress =  new TransportAddress(InetAddress.getByName(EsHost), EsPort);
+        Client client =  preBuiltTransportClient.addTransportAddress(inetSocketTransportAddress);
+
+
+        return client;
+//        return TransportClient.builder()
+//                .settings(esSettings)
+//                .build()
+//                .addTransportAddress(
+//                        new InetSocketTransportAddress(InetAddress.getByName(EsHost), EsPort));
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() throws Exception {
+        return new ElasticsearchTemplate(client());
+    }
+
+    //Embedded Elasticsearch Server
+    /*@Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchTemplate(nodeBuilder().local(true).node().client());
+    }*/
+}
